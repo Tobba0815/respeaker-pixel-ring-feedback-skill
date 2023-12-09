bf3c1378b1d1e840acf543a493f081e7e7b070b7
Tobba <tobias.haase@lichtfeld.com>
2023-12-09 13:18:46 +0100

    add intents; add dialogues; add basic logic; extend pixels logic; modify dependency definition; set settings metadata

--- __init__.py
+++ __init__.py
@@ -1,15 +1,117 @@
 from mycroft import MycroftSkill, intent_file_handler
+from mycroft_bus_client import MessageBusClient, Message
 
+from .lib.pixels import Pixels
 
-class RespeakerPixelRingFeedback(MycroftSkill):
+class ReSpeakerPixelFeedback(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_file_handler('feedback.ring.pixel.respeaker.intent')
-    def handle_feedback_ring_pixel_respeaker(self, message):
-        self.speak_dialog('feedback.ring.pixel.respeaker')
+    def initialize(self):
+        self.add_events()
+        self.use_settings()
 
+        self.pixels = Pixels(self.pixel_pattern, self.device_num_pixels)
+
+    def use_settings(self):
+        self.device_type = self.settings.get('device_type')
+        self.device_num_pixels = self.settings.get('device_num_pixels')
+        self.pixel_pattern = self.settings.get('pixel_pattern')
+
+    def on_settings_changed(self):
+        self.use_settings()
+
+    def add_events(self):
+        # add events for basic recognition
+        self.add_event('recognizer_loop:wakeword', self.handle_wakeup)
+        self.add_event('recognizer_loop:record_begin', self.handle_output)
+        self.add_event('recognizer_loop:record_end', self.handle_stop)
+        self.add_event('recognizer_loop:audio_output_start', self.handle_output)
+        self.add_event('recognizer_loop:audio_output_end', self.handle_stop)
+        # add events for question skill
+        self.add_event('question.query', self.handle_processing)
+        self.add_event('question.query.response', self.handle_output)
+        # add events for audio
+        self.add_event('mycroft.audio.service.play', self.handle_output)
+        self.add_event('mycroft.audio.service.stop', self.handle_stop)
+        self.add_event('mycroft.audio.service.pause', self.handle_stop)
+        self.add_event('mycroft.audio.service.resume', self.handle_output)
+        # add events for skill handlers
+        self.add_event('mycroft.skill.handler.start', self.handle_processing)
+        self.add_event('mycroft.skill.handler.complete', self.handle_stop)
+        # other events
+        self.add_event('complete_intent_failure', self.handle_stop)
+
+    def handle_listen(self, message):
+        self.pixels.listen()
+
+    def handle_wakeup(self, message):
+        self.pixels.wakeup()
+
+    def handle_processing(self, message):
+        self.pixels.think()
+
+    def handle_output(self, message):
+        self.pixels.speak()
+
+    def handle_stop(self, message):
+        self.pixels.off()
+
+    @intent_file_handler('pixel.get.pattern.intent')
+    def handle_pixel_get_pattern(self, message):
+        self.speak_dialog('pixel.get.pattern', {pattern: self.pixel_pattern})
+        pass
+
+    @intent_file_handler('pixel.set.pattern.intent')
+    def handle_pixel_set_pattern(self, message):
+        pattern = message.data.get('pattern')
+        if pattern is not None:
+            # check patterns
+            if pattern in ['alexa', 'google']:
+                self.pixel_pattern = pattern
+                self.speak_dialog('pixel.set.pattern', {pattern: self.pixel_pattern})
+            else:
+                self.speak_dialog('pixel.set.pattern.fail', {pattern: pattern})
+        else:
+            self.speak.dialog('pixel.set.pattern.fail.unknown')
+
+    @intent_file_handler('pixel.show.intent')
+    def handle_pixel_show(self, message):
+        kind = message.data.get('kind')
+        if kind is not None:
+            # todo: translate kind
+            if kind in ['wakeup', 'listen', 'think', 'process', 'speak']:
+                if kind == 'wakeup':
+                    self.pixel.wakeup()
+                    time.sleep(3)
+                    self.pixel.stop()
+
+                elif kind == 'listen':
+                    self.pixel.listen()
+                    time.sleep(3)
+                    self.pixel.stop()
+
+                elif kind == 'think' or kind == 'process':
+                    self.pixel.think()
+                    time.sleep(3)
+                    self.pixel.stop()
+
+                elif kind == 'speak' or kind == 'output':
+                    self.pixel.speak()
+                    time.sleep(3)
+                    self.pixel.stop()
+
+                else:
+                    self.speak_dialog('pixel.show.fail', {kind: kind})
+            else:
+                self.speak_dialog('pixel.show.fail', {kind: kind})
+        else:
+            self.speak_dialog('pixel.show.fail.unknown')
+
+    @intent_file_handler('pixel.stop.intent')
+    def pixel_stop_handler(self, message):
+        self.handle_stop()
 
 def create_skill():
-    return RespeakerPixelRingFeedback()
+    return ReSpeakerPixelFeedback()
 
--- lib/pixels.py
+++ lib/pixels.py
@@ -1,4 +1,3 @@
-
 import time
 import threading
 from gpiozero import LED
@@ -8,17 +7,21 @@ except ImportError:
     import Queue as Queue
 
 from .apa102 import APA102
-from pattern.alexa_led_pattern import AlexaLedPattern
-from pattern.google_home_led_pattern import GoogleHomeLedPattern
+from .pattern.alexa_led_pattern import AlexaLedPattern
+from .pattern.google_home_led_pattern import GoogleHomeLedPattern
 
 class Pixels:
-    PIXELS_N = 12
+    num_pixels = None
+    controller = None
+    patterns = {
+        'google': GoogleHomeLedPattern,
+        'alexa': AlexaLedPattern
+    }
+    def __init__(self, pattern="google", num_pixels=12):
 
-    def __init__(self, pattern=GoogleHomeLedPattern):
-
-        self.pattern = pattern(show=self.show)
-
-        self.dev = APA102(num_led=self.PIXELS_N)
+        self.set_num_pixels(num_pixels)
+        if self.set_pattern(pattern) == False:
+            raise RuntimeError('pattern "{}" is not defined'.format(pattern))
 
         self.power = LED(5)
         self.power.on()
@@ -30,6 +33,39 @@ class Pixels:
 
         self.last_direction = None
 
+    def set_pattern(self, pattern_name):
+        if pattern_name in self.patterns:
+            pattern = self.patterns.get(pattern_name)
+            self.pattern = pattern(show=self.show)
+            return True
+        else:
+            return False
+
+    def set_num_pixels(self, num_pixels):
+        if self.controller is not None:
+            self.controller.cleanup()
+
+        self.num_pixels = num_pixels
+        self.controller = APA102(num_led=num_pixels)
+
+    ### generic feedback functions
+    def info(self):
+        #todo: show pixel feedback in lightblue
+        pass
+
+    def success(self):
+        #todo: show pixel feedback in green
+        pass
+
+    def warning(self):
+        #todo: show pixel feedback in yellow/orange
+        pass
+
+    def error(self):
+        #todo: show pixel feedback in red
+        pass
+
+    ### pattern feedback functions
     def wakeup(self, direction=0):
         self.last_direction = direction
         def f():
@@ -65,7 +101,7 @@ class Pixels:
             func()
 
     def show(self, data):
-        for i in range(self.PIXELS_N):
-            self.dev.set_pixel(i, int(data[4*i + 1]), int(data[4*i + 2]), int(data[4*i + 3]))
+        for i in range(self.num_pixels):
+            self.controller.set_pixel(i, int(data[4*i + 1]), int(data[4*i + 2]), int(data[4*i + 3]))
 
-        self.dev.show()
+        self.controller.show()
